84dd0ee38e2f93882af78a2c36aa40976f49cba4
hbenbow <harriet.benbow@gmail.com>
2020-05-21 15:20:18 +0100

    combined read count stats with data input files

--- read_count_stats.R
+++ read_count_stats.R
@@ -4,6 +4,7 @@ library(DESeq2)
 library(ggplot2)
 library(dplyr)
 library(plyr)
+library(tidyr)
 allowWGCNAThreads()
 
 # Import kallisto files with txi 
@@ -29,11 +30,22 @@ dds <- DESeqDataSetFromTximport(txi.kallisto.tsv, colData, ~ Treatment + Timepoi
 
 
 # Identify numbers of expressed transcripts
-dds <- estimateSizeFactors(dds)
-idx <- rowSums(counts(dds, normalized=TRUE) >= .5 ) >=2
-dds <- dds[idx,]
+# dds <- estimateSizeFactors(dds)
+# idx <- rowSums(counts(dds, normalized=TRUE) >= .5 ) >=2
+# dds <- dds[idx,]
 # This would filter out genes where there are fewer 
 # than 6 samples with normalized counts greater than or equal to .5. 
 # Chose 6 as it is 1/8 of the samples (i.e. just treated in one genotype).
+vst<-varianceStabilizingTransformation(dds)
 
-expressed_genes<-assay(dds)
+expressed_genes$GeneID<-row.names(expressed_genes)
+expressed_genes<- expressed_genes[- grep("LC", expressed_genes$GeneID),]
+expressed_genes<-expressed_genes[,c(49, 1:48)]
+expressed_genes_long<-expressed_genes %>% gather(Sample, TPM, 2:49)
+all_wheat_genes<-merge(expressed_genes_long, colData, by="Sample")
+
+test<-spread(test, Rep, TPM)
+test<-aggregate(all_wheat_genes$TPM, by=list(all_wheat_genes$TPM, all_wheat_genes$GeneID), FUN="sum")
+
+expressed_genes_filtered<-expressed_genes_long[(expressed_genes_long$TPM>=0.5),]
+length(unique(expressed_genes_filtered$GeneID))
