9dcef017532aea153604fd2d0b2dec52cd982f58
hbenbow <harriet.benbow@gmail.com>
2020-05-26 15:23:12 +0100

    added plots for chromosome and sample distribution of read counts

--- data_input_and_read_count_stats.R
+++ data_input_and_read_count_stats.R
@@ -10,6 +10,8 @@ library(gplots)
 library(tidyr)
 library(Hmisc)
 library(corrplot)
+library(pheatmap)
+library(RColorBrewer)
 
 allowWGCNAThreads()
 
@@ -26,11 +28,12 @@ txi.kallisto.tsv <- tximport(files, type = "kallisto", countsFromAbundance = "sc
 colData<-read.csv("~/Documents/bmc/Data/colData.csv")
 setwd("~/Documents/bmc/Data/")
 colData$Timepoint<-as.factor(colData$Timepoint)
+colData$Rep<-as.factor(colData$Rep)
 # ==================================================================================
 
 
 # if already have a txi object, load it with the metadata (colData)
-load("~/Documents/bmc/Data/txi_and_colData.RData")
+# load("~/Documents/bmc/Data/txi_and_colData.RData")
 
 # check that order of samples in metadata and txi object are the same
 order<-colData$Sample
@@ -61,6 +64,43 @@ corrplot(cor$r, type="lower", order="original",p.mat = cor$P,
          sig.level = 0.05, insig = "blank", tl.col="black", tl.cex = 2, 
          tl.srt = 0, tl.offset = 1, method="color", addCoef.col = "white")
 
+rep_wise$sum2<-rep_wise$test1 + rep_wise$test2 + rep_wise$test3
+expressed<-rep_wise[(rep_wise$sum2 >=2),] 
+length(unique(expressed$GeneID))
+unique_tx<-as.data.frame(unique(expressed$GeneID))
+colnames(unique_tx)<-"GeneID"
+unique_tx$Gene<-unique_tx$GeneID
+unique_gene<- unique_tx %>% 
+  separate(Gene, c("Gene", "Transcript"))
+unique_gene$Tx<-unique_gene$GeneID
+unique_gene<- unique_gene %>% 
+  separate(Tx, c("dead", "Chromosome"), sep="S")
+unique_gene$dead<-NULL
+unique_gene <-unique_gene %>%
+  separate(Chromosome, into=c("Chrom", "dead"), sep="0", extra="drop")
+unique_gene$dead<-NULL
+unique_gene$Chrom<-str_replace(unique_gene$Chrom, "U", "UU")
+chroms<-unique_gene$Chrom
+test<-strsplit(chroms, split="")
+chroms<-do.call(rbind.data.frame, test)
+colnames(chroms)<-c("Chromosome", "Genome")
+unique_gene<-cbind(unique_gene, chroms)
+tab<-
+  as.data.frame(table(unique_gene$Chromosome, unique_gene$Genome))
+colnames(tab)<-c("Chromosome", "Genome", "Frequency")
+
+ggplot(tab, aes(x=Chromosome, y=Frequency)) + 
+  geom_col(aes(fill=Genome)) +
+  theme_classic() +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme(text = element_text(size=20, colour="black"), 
+        axis.text.x = element_text(colour="black")) +
+  labs(fill = "Genome") +
+  xlab("Chromosome") +
+  ylab("Number of expressed genes / gene isoforms")+
+  scale_fill_manual(values=c("grey80", "grey60", "grey40", "grey20"))
+
+
 table<-merge(as.data.frame(table(expressed$Factor)),colData, by.x="Var1", by.y="Factor") 
 
 ggplot(table, aes(x=as.factor(Timepoint), y=Freq)) +
@@ -69,26 +109,16 @@ ggplot(table, aes(x=as.factor(Timepoint), y=Freq)) +
   facet_grid(Genotype~.) +
   theme(legend.position = "right") + 
   scale_fill_manual(values=c("grey80","grey40"), labels=c("Tween20", expression(paste(italic("Z. tritici"))))) + 
-  ylab("Number of expressed genes") + 
+  ylab("Number of expressed genes / gene isoforms") + 
   xlab("Timepoint (dpi)")+
   theme(text = element_text(size=20, colour="black"), 
         axis.text.x = element_text(colour="black")) +
   scale_y_continuous(expand=c(0,0)) +
-  geom_hline(aes(yintercept=0))
-
-test<-aggregate(all_wheat_genes$TPM, by=list(all_wheat_genes$Factor, all_wheat_genes$GeneID, all_wheat_genes$Rep), FUN="sum")
-all_wheat_genes<-all_wheat_genes[,c(9, 1,2,3,4,5,6,7,8)]
-
-
-expressed_genes_filtered<-expressed_genes_long[(expressed_genes_long$TPM>0.5),]
-expressed_genes_filtered$Gene<-expressed_genes_filtered$GeneID
-expressed_genes_filtered<-separate(expressed_genes_filtered, Gene, c("Gene", "Transcript"), "\\.")
-length(unique(expressed_genes_filtered$GeneID))
-length(unique(expressed_genes_filtered$Gene))
+  geom_hline(aes(yintercept=0)) 
 
 
 # colData is metadata with factor column. Make dds object with deseq
-dds <- DESeqDataSetFromTximport(txi.kallisto.tsv, colData, ~ Treatment + Timepoint + Genotype + Treatment:Genotype)
+dds <- DESeqDataSetFromTximport(txi.kallisto.tsv, colData, ~ Treatment + Timepoint + Genotype +  Rep + Treatment:Genotype)
 # transform using variance stabilising transformation
 vsd <- varianceStabilizingTransformation(dds, blind=FALSE)
 
@@ -96,12 +126,32 @@ vsd <- varianceStabilizingTransformation(dds, blind=FALSE)
 pcaData <- plotPCA(vsd, intgroup=c("Treatment", "Timepoint", "Genotype", "Rep"), returnData=TRUE)
 
 # plot PC1 vs PC2
-ggplot(pcaData, aes(x=PC1, y=PC2)) + geom_point(aes(colour=Genotype, shape=Treatment), size=4, alpha=0.7) +
+ggplot(pcaData, aes(x=PC1, y=PC2)) + geom_point(aes(colour=Genotype, shape=Treatment), size=5, alpha=0.7) +
   theme_classic() +
   theme(text = element_text(size=20, colour="black"),
         axis.text.x = element_text(colour="black")) +
-  scale_colour_manual(values=c("black","grey50"))
+  scale_colour_manual(values=c("black","grey50")) +
+  scale_shape_manual(values=c("circle", "square"), labels=c("Tween20", expression(paste(italic("Z. tritici")))))+
+  xlab("Principle component 1")+
+  ylab("Principle component 2")
 
+
+ # heatmap of genes by sample
+dds<-estimateSizeFactors(dds)
+select <- order(rowMeans(counts(dds,normalized=TRUE)),
+                decreasing=TRUE)[1:100]
+df <- as.data.frame(colData(dds)[,c("Genotype", "Treatment")])
+pheatmap(assay(dds)[select,], cluster_rows=FALSE, show_rownames=FALSE,
+         cluster_cols=FALSE, annotation_col=df)
+
+# bheatmap of sample distances
 vst_counts<-assay(vsd)
-
-
+sampleDists <- dist(t(assay(vsd)))
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- paste(vsd$Rep, sep="-")
+colnames(sampleDistMatrix) <- NULL
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
