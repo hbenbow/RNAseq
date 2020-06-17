db377f0796006e5004fa6c026ac9ec8c9f230cc6
hbenbow <harriet.benbow@gmail.com>
2020-06-17 17:39:40 +0100

    extract stigg genes

--- compare_genotypes.R
+++ compare_genotypes.R
@@ -87,57 +87,52 @@ all_compare_geno<-rbind(REST6, REST24, REST48, REST96, RESC6, RESC24, RESC48, RE
 all_sig_genotype<-all_compare_geno[(all_compare_geno$padj<0.05),]
 all_sig_genotype<-all_sig_genotype[abs(all_sig_genotype$log2FoldChange)>.5,]
 all_sig_genotype<-na.omit(all_sig_genotype)
+write.csv(all_sig_genotype, file="~/Documents/bmc/Data/all_significant_by_genotype.csv")
+# treated only DEGs
+all_sig_genotype_genes<-as.character(unique(all_sig_genotype$row))
+geno_and_treatment<-subset(all_significant, all_significant$ID %in% all_sig_genotype_genes)
 
-assay<-assay(dds2)
-vst<-varianceStabilizingTransformation(dds2)
-vds<-assay(vst)
+all_significant$Factor<-paste(all_significant$Cultivar, all_significant$Timepoint, all_significant$Regulation)
+Factor="Stigg 6 Down"
 
-
-counts1<-vds
-# counts1<-vds
-counts<-counts1
-colData$TxGxT<-paste(colData$Rep, colData$Genotype, colData$Timepoint)
-colData$Factor2<-paste(colData$Treatment, colData$Timepoint)
-
-
-TPM<-as.data.frame(counts1)
-genes<-row.names(TPM)
-TPM<-gather(TPM, key="Sample", value="TPM")
-TPM$GeneID<-genes
-TPM<-merge(TPM, colData, by="Sample")
-TPM$Factor<-paste(TPM$GeneID, TPM$Rep, TPM$Timepoint, TPM$Genotype)
-TPM2<-TPM[,c(9, 2, 5)]
-TPM3<-spread(TPM2, key="Treatment", value="TPM")
-TPM3$FoldChange<-(TPM3$T)/(TPM3$C)
-metadata<-TPM[TPM$Treatment=="T",]
-metadata<-metadata[,c(9, 3, 4, 6, 7)]
-TPM4<-merge(TPM3, metadata, by="Factor")
-TPM4$Log2FoldChange<-sqrt(TPM4$FoldChange)
-
-
-all_significant$Factor<-paste(all_significant$Timepoint, all_significant$Regulation)
-test<-list()
-for(factor in unique(all_significant$Factor)){
-  data<-all_significant[(all_significant$Factor==factor),]
-  time<-unique(data$Timepoint)
-  DEGs<-as.character(unique(data$gene))
-  degs_tpm<-subset(TPM4, TPM4$GeneID %in% DEGs)
-  degs_tpm<-subset(degs_tpm, degs_tpm$Timepoint %in% time)
-  list<-list()
-  for(gene in unique(degs_tpm$GeneID)){
-    data<-degs_tpm[(degs_tpm$GeneID==gene),]
-    kt<-kruskal.test(FoldChange ~ Genotype, data)[[3]]
-    kt<-as.data.frame(cbind(unique(data$GeneID), kt))
-    list[[(length(list)+1)]]<-kt
-  }
-  krusk<-as.data.frame(do.call("rbind", list))
-  krusk$data<-paste(factor)
-  test[[(length(test)+1)]]<-krusk
+list<-list()
+for(i in as.character(unique(all_significant$Factor))){
+data<-all_significant[(all_significant$Factor==i),]
+timepoint<-as.character(unique(data$Timepoint))
+regulation<-as.character(unique(data$Regulation))
+genos<-all_sig_genotype[(all_sig_genotype$Timepoint==timepoint),]
+genos<-subset(genos, genos$row %in% data$ID)
+chroms<-subset(all_significant, all_significant$ID %in% genos$row)
+chroms<-chroms[!(duplicated(chroms$ID)),]
+chroms<-chroms[,c(9, 12, 13, 14)]
+genos<-merge(genos, chroms, by.x="row", by.y="ID", all.x=T, all.y=F)
+t<-as.data.frame(table(genos$row, genos$Treatment, genos$Genome))
+t<-spread(t, key="Var2", value="Freq")
+tonly<-t[(t$Treated==1),]
+tonly<-tonly[(tonly$Control==0),]
+tonly<-spread(as.data.frame(table(tonly$Var3)), key="Var1", value="Freq")
+tonly$Factor<-i
+tonly$Treatment<-"Treated"
+conly<-t[(t$Control==1),]
+conly<-conly[(conly$Treated==0),]
+conly<-spread(as.data.frame(table(conly$Var3)), key="Var1", value="Freq")
+conly$Factor<-i
+conly$Treatment<-"Control"
+both<-t[(t$Control==1),]
+both<-both[(both$Treated==1),]
+both<-spread(as.data.frame(table(both$Var3)), key="Var1", value="Freq")
+both$Factor<-i
+both$Treatment<-"Both"
+d2<-as.data.frame(rbind(tonly, conly, both))
+list[[(length(list)+1)]]<-d2
 }
 
-all<-as.data.frame(do.call("rbind", test))
+table<-as.data.frame(do.call(rbind.data.frame, list))
+write.csv(table, file="~/Documents/bmc/Data/DE_genes/table_of_degs.csv")
 
-write.csv(all, file="~/Documents/bmc/Data/compare_genotypes2.csv")
-
-all_foldchange<-subset(TPM4, TPM4$GeneID %in% all$V1)
-
+Stigg_DEGs<-as.character(unique(all_significant[(all_significant$Cultivar=="Stigg"),9]))
+stigg_DEGs_geno<-subset(all_sig_genotype, all_sig_genotype$row %in% Stigg_DEGs)
+chroms<-subset(all_significant, all_significant$ID %in% Stigg_DEGs)
+chroms<-chroms[!(duplicated(chroms$ID)),]
+chroms<-chroms[,c(9, 12, 13, 14)]
+genos<-merge(stigg_DEGs_geno, chroms, by.x="row", by.y="ID", all.x=T, all.y=F)
